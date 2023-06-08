eae5f464334e5c4f162459f0834ff2e35bf05257
Alexis Sayago <as.sayagom@gmail.com>
2023-06-08 17:31:13 -0400

    some features added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+const [task, setTask] = useState("")
+const handleChange = (event) => {
+    setTask(event.target.value)
+  }
+
+const [listOfTasks, setListOfTasks] = useState([])
+const addingTask = (event) => {
+	if (event.key === "Enter"){
+		setListOfTasks(listOfTasks.concat([task]));
+		setTask("");
+	}
+}
+
   return (
-    <div id="container">
-			<h1 className="todo-header">To do List</h1>
-			<input id="addToDo" type="text" placeholder="Add to do here" />
+	<div id="container">
+		<h1 className="todo-header">To do List</h1>
+			<input id="addToDo" type="text" placeholder="Add to do here" onChange={handleChange} value={task} onKeyUp={addingTask}/>
 			<ul>
-				<li>
-					<span><i className="fa fa-trash"></i></span> Eat
-				</li>
-				<li>
-					<span><i className="fa fa-trash"></i></span> Drink
-				</li>
-				<li>
-					<span><i className="fa fa-trash"></i></span> Sleep
-				</li>
+				<li><span><i className="fa fa-trash"></i></span>{task}</li>
+				{listOfTasks.map((item, index)=>{<li><span><i className="fa fa-trash" onClick={()=>setListOfTasks(listOfTasks.filter((itemToBeEliminated, currentIndex)=>index!= currentIndex))}></i></span>{item}{" "}</li>})}
 			</ul>
-		</div>
+		<div>{listOfTasks.length+" tasks left"}</div>
+	</div>
   );
 }
 
