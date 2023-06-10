029bbabd3587f7da75ae73600bc40f520e700360
Alexis Sayago <as.sayagom@gmail.com>
2023-06-09 20:22:08 -0400

    project working

--- src/App.js
+++ src/App.js
@@ -2,30 +2,34 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-const [task, setTask] = useState("")
-const handleChange = (event) => {
-    setTask(event.target.value)
-  }
+	const [task, setTask] = useState("")
+	const [listOfTasks, setListOfTasks] = useState([])
 
-const [listOfTasks, setListOfTasks] = useState([])
-const addingTask = (event) => {
-	if (event.key === "Enter"){
-		setListOfTasks(listOfTasks.concat([task]));
-		setTask("");
+	const handleChange = (event) => {
+		setTask(event.target.value)
 	}
-}
 
-  return (
-	<div id="container">
-		<h1 className="todo-header">To do List</h1>
-			<input id="addToDo" type="text" placeholder="Add to do here" onChange={handleChange} value={task} onKeyUp={addingTask}/>
+	const addingTask = (event) => {
+		if (event.key === "Enter") {
+			setListOfTasks(listOfTasks.concat([task]));
+			setTask("");
+		}
+	}
+
+	return (
+		<div id="container">
+			<h1 className="todo-header">To do List</h1>
+			<input id="addToDo" type="text" placeholder="Add to do here" onChange={handleChange} value={task} onKeyUp={addingTask} />
 			<ul>
-				<li><span><i className="fa fa-trash"></i></span>{task}</li>
-				{listOfTasks.map((item, index)=>{<li><span><i className="fa fa-trash" onClick={()=>setListOfTasks(listOfTasks.filter((itemToBeEliminated, currentIndex)=>index!= currentIndex))}></i></span>{item}{" "}</li>})}
+				{ listOfTasks.length===0 ? <li>No tasks remaining</li> :
+				listOfTasks.map((item, index) => {
+					 return <li key={index}><span><i className="fa fa-trash" onClick={() => setListOfTasks(listOfTasks.filter((itemToBeEliminated, currentIndex) => index !== currentIndex))}></i></span>{item}{" "}</li>
+				})
+				}
 			</ul>
-		<div>{listOfTasks.length+" tasks left"}</div>
-	</div>
-  );
+			<div>{listOfTasks.length!==0 ? (listOfTasks.length === 1 ? listOfTasks.length + " task left" : listOfTasks.length + " tasks left") : null}</div>
+		</div>
+	);
 }
 
 export default App;
