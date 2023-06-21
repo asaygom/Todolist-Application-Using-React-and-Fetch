2e769b8f56e6d1de802630f6357f65fa45d6d9a0
Alexis Sayago <as.sayagom@gmail.com>
2023-06-21 15:38:58 -0400

    update of list of tasks working and some visual changes

--- src/App.js
+++ src/App.js
@@ -2,25 +2,27 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-	const [task, setTask] = useState("")
+	const [task, setTask] = useState({label:""})
 	const [listOfTasks, setListOfTasks] = useState([])
 	const [username, setUsername] = useState("")
 	const [listOfUsernames, setListOfUsernames] = useState([])
-	const [listOfTasksFF, setListOfTasksFF] = useState([])
 
 	const handleChange = (event) => {
-		setTask(event.target.value)
+		setTask(task =>({
+			...task, label: event.target.value, done: false
+		}))
 	}
 
 	const addingTask = (event) => {
 		if (event.key === "Enter") {
 			setListOfTasks(listOfTasks.concat([task]));
-			setTask("");
+			setTask({label:""});
 		}
 	}
 
 	const handleUsername = (event) => {
 		setUsername(event.target.value)
+		event.preventDefault()
 	}
 
 	fetch("http://assets.breatheco.de/apis/fake/todos/user", {
@@ -34,7 +36,6 @@ function App() {
 		console.log(error)
 	})
 
-
 	useEffect(()=>{if(username!==""&&username!=="Choose user"){
 		fetch("http://assets.breatheco.de/apis/fake/todos/user/"+username,{
 			method:"GET",
@@ -42,7 +43,7 @@ function App() {
 		}).then((response)=>{
 			return response.json()
 		}).then((data)=>{
-			setListOfTasksFF(data)
+			setListOfTasks(data)
 		}).catch((error)=>{
 			console.log(error)
 		})
@@ -65,7 +66,7 @@ function App() {
 	const updateList = () => {
 		fetch("http://assets.breatheco.de/apis/fake/todos/user/"+username,{
 			method:"PUT",
-			body: JSON.stringify(listOfTasksFF),
+			body: JSON.stringify(listOfTasks),
 			headers:{"Content-Type":"application/json"}
 		}).then((response)=>{
 			return response.json()
@@ -74,45 +75,57 @@ function App() {
 		}).catch((error)=>{
 			console.log(error)
 		})
-		setUsername("")
+		
+	}
+
+	const createUser = (event) => {
+		if (event.key === "Enter") {
+			fetch("http://assets.breatheco.de/apis/fake/todos/user/"+username,{
+				method:"POST",
+				body: JSON.stringify("[]"),
+				headers:{"Content-Type":"application/json"}
+			}).then((response)=>{
+				return response.json()
+			}).then((data)=>{
+				console.log(data)
+			}).catch((error)=>{
+				console.log(error)
+			})
+			
+		}
 	}
 
 	return (
 		<div>
 			<div className='header'>
-				<select className='selection' onChange={handleUsername} value={username}>
+				<select className='form-select' onChange={handleUsername} value={username}>
 					<option>Choose user</option>
 					{listOfUsernames.map((item, index)=>{ return <option key={index}>{item}</option>})}
 				</select>
+				<span className="form-label">OR</span>
+				<div className="input-group">
+					<input id="createUser" type="text" className="form-control" placeholder="Recipient's username" onChange={handleUsername} value={username} aria-label="Recipient's username" aria-describedby="button-addon2" />
+					<button className="btn" type="button" id="button-addon2" onClick={createUser}>Create user</button>
+				</div>
 			</div>
 			<div id="container">
 				<h1 className="todo-header">To do List</h1>
-				<input id="addToDo" type="text" placeholder="Add to do here" onChange={handleChange} value={task} onKeyUp={addingTask} />
+				<input id="addToDo" type="text" placeholder="Add to do here" onChange={handleChange} value={task.label} onKeyUp={addingTask} />
 				<ul>
-					{ listOfTasksFF.length===0 ? <li>No tasks remaining</li> :
-					listOfTasksFF.map((item, index) => {
-						return <li key={index}>{item.label}</li>
+					{ listOfTasks.length===0 ? <li>No tasks remaining</li> :
+					listOfTasks.map((item, index) => {
+						return <li key={index}><span className="trash" onClick={() => setListOfTasks(listOfTasks.filter((itemToBeEliminated, currentIndex) => index !== currentIndex))}><i className="fa fa-trash"></i></span>{item.label}</li>
 					})
 				}
 				</ul>
-				<div>{listOfTasksFF.length!==0 ? (listOfTasksFF.length === 1 ? listOfTasksFF.length + " task left" : listOfTasksFF.length + " tasks left") : null}</div>
+				<div>{listOfTasks.length!==0 && (listOfTasks.length === 1 ? listOfTasks.length + " task left" : listOfTasks.length + " tasks left")}</div>
 			</div>
 			<div className='footer'>
-				<button className="button" onClick={updateList}>Update list of tasks</button>
-				<button className="button2" onClick={deleteUsername}>Delete user and tasks</button>
+				<button id="button" className="btn" onClick={updateList}>Update list of tasks</button>
+				<button id="button2" className="btn" onClick={deleteUsername}>Delete user and tasks</button>
 			</div>
 		</div>
 	);
 }
 
 export default App;
-/* 
-<ul>
-					{ listOfTasks.length===0 ? <li>No tasks remaining</li> :
-					listOfTasks.map((item, index) => {
-						return <li key={index}><span><i className="fa fa-trash" onClick={() => setListOfTasks(listOfTasks.filter((itemToBeEliminated, currentIndex) => index !== currentIndex))}></i></span>{item}{" "}</li>
-					})
-					}
-				</ul>
-				<div>{listOfTasks.length!==0 ? (listOfTasks.length === 1 ? listOfTasks.length + " task left" : listOfTasks.length + " tasks left") : null}</div>
-				*/
